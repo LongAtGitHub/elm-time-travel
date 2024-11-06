24a091d5af918e1f3a2e5d3d78ccb78eba4e4cad
LongAtGitHub <98071520+LongAtGitHub@users.noreply.github.com>
2024-11-06 14:35:37 -0600

    advancing into final step

--- src/TimeTravel.elm
+++ src/TimeTravel.elm
@@ -3,15 +3,20 @@ module TimeTravel exposing (addTimeTravel)
 import Playground exposing (..)
 import Set
 
+maxVisibleHistory = 2000
+
 addTimeTravel rawGame =
   { initialState = initialStateWithTimeTravel rawGame
   , updateState = updateWithTimeTravel rawGame
   , view = viewWithTimeTravel rawGame
   }
 
+-- model
 initialStateWithTimeTravel rawGame =
   { rawModel = rawGame.initialState
   , paused = False
+  , history = []
+  , historyPlaybackPosition = 0
   }
 
 viewWithTimeTravel rawGame computer model =
@@ -21,13 +26,34 @@ viewWithTimeTravel rawGame computer model =
           "Press R to resume"
         else
           "Press T to time travel"
+    historyBar color opacity index =
+      let
+        width = historyIndexToX computer index
+      in
+        rectangle color width controlBarHeight  
+          |> move (computer.screen.left + width / 2)
+                  (computer.screen.top - controlBarHeight / 2)
+          |> fade opacity
   in
     (rawGame.view computer model.rawModel) ++
-      [ words white helpMessage
+      [ 
+        historyBar black 0.3 maxVisibleHistory,
+        historyBar grey 0.6 (List.length model.history),
+        historyBar blue 0.5 (model.historyPlaybackPosition),
+        words white helpMessage
           |> move 0 (computer.screen.top - controlBarHeight / 2)
       ]
 
 updateWithTimeTravel rawGame computer model =
+    if (model.paused ) && ( computer.mouse.down ) then
+    let 
+        newPlaybackPosition = min (mousePosToHistoryIndex computer) (List.length model.history)
+    in
+        { model
+        | historyPlaybackPosition = newPlaybackPosition
+        }
+    else
+    
     if keyPressed "T" computer then
         { model | paused = True }
     else if keyPressed "R" computer then
@@ -35,7 +61,11 @@ updateWithTimeTravel rawGame computer model =
     else if model.paused then
         model
     else
-        { model | rawModel = rawGame.updateState computer model.rawModel }
+        { model | rawModel = rawGame.updateState computer model.rawModel, history= model.history ++[computer], historyPlaybackPosition = List.length model.history+1 }
+
+-- Converts an index in the history list to an x coordinate on the screen
+historyIndexToX computer index =
+  (toFloat index) / maxVisibleHistory * computer.screen.width
 
 keyPressed keyName computer =
   [ String.toLower keyName
@@ -44,3 +74,9 @@ keyPressed keyName computer =
     |> List.any (\key -> Set.member key computer.keyboard.keys)
 
 controlBarHeight = 64
+
+-- Converts the mouse's current position to an index within the history list
+mousePosToHistoryIndex computer =
+  (computer.mouse.x - computer.screen.left)
+    / computer.screen.width * maxVisibleHistory
+  |> round
